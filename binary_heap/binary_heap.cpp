89b9e464181b6c669080f37ca4ccf201fbcde122
Dmitry Yutkin <yutkinn@gmail.com>
2016-10-19 23:25:50 +0300

    Added heap sort

--- binary_heap/binary_heap.cpp
+++ binary_heap/binary_heap.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
-
 #include "binary_heap.h"
 
 int main() {
   
-  auto init_list = {5, 9, 1, 8, -999, 6, 35, -2, 22, -12, 129, 0, 3 , 6, 2, 1, 123};
-  
-  custom::BinaryHeap<int, std::less<int>> maxHeap(init_list.begin(), init_list.end());
-  custom::BinaryHeap<int, std::greater<int>> minHeap = init_list;
-  
+  std::vector<int> v = {5, 9, 1, 8, -999, 6, 35, -2, 22, -12, 129, 0, 3 , 6, 2, 1, 123};
 
-  assert(maxHeap.size() == minHeap.size());
-  assert(maxHeap.top() == 129);
-  assert(minHeap.top() == -999);
+  custom::heapify(v.begin(), v.end(), std::greater<int>());
+  std::cout << "Min-Heapified vector:\n";
+  for (int i = 0; i < v.size(); ++i) {
+    std::cout << v[i] << ' ';
+  }
+  std::cout << std::endl;
+  
+  custom::BinaryHeap<int, std::less<int>> maxHeap(v.begin(), v.end());
+  
+  assert(maxHeap.top() == *std::max_element(v.begin(), v.end()));
   
   std::cout << "Max BinHeap:\n";
   while (!maxHeap.empty()) {
@@ -21,10 +23,14 @@ int main() {
   }
   std::cout << std::endl;
 
-  std::cout << "\nMin BinHeap:\n";
-  while (!minHeap.empty()) {
-    std::cout << minHeap.top() << ' ';
-    minHeap.pop();
+  std::vector<int> v2 = {5, 9, 1, 8, -999, 6, 35, -2, 22, -12, 129, 0, 3 , 6, 2, 1, 123};
+  custom::heap_sort(v2.begin(), v2.end());
+
+  assert(std::is_sorted(v2.begin(), v2.end()));
+
+  std::cout << "Sorted by heap sort:\n";
+  for (auto& i : v2) {
+    std::cout << i << ' ';
   }
   std::cout << std::endl;
   return 0;
--- binary_heap/binary_heap.h
+++ binary_heap/binary_heap.h
@@ -1,5 +1,5 @@
-#ifndef PRIORITY_QUEUE_H
-#define PRIORITY_QUEUE_H
+#ifndef BINARY_HEAP_H
+#define BINARY_HEAP_H
 
 #include <functional>
 #include <vector>
@@ -7,10 +7,12 @@
 #include <initializer_list>
 
 using std::swap;
+using std::function;
+using std::less;
 
 namespace custom {
 
-template<typename DataType, typename Predicate=std::less<DataType> >
+template<typename DataType, typename Predicate=less<DataType> >
 class BinaryHeap {
 public:
   explicit BinaryHeap() {}
@@ -67,19 +69,22 @@ private:
   }
 
   void siftDown(size_t i) {
-    auto maxInd = i;
-    
-    auto lChild = leftChild(i);
-    if (lChild < heap_size && compare(heap[maxInd], heap[lChild]))
-      maxInd = lChild;
-    
-    auto rChild = rightChild(i);
-    if (rChild < heap_size && compare(heap[maxInd], heap[rChild]))
-      maxInd = rChild;
+    while (leftChild(i) < heap_size) {
+      auto maxInd = i;
+      
+      auto lChild = leftChild(i);
+      auto rChild = rightChild(i);
 
-    if (i != maxInd) {
-      swap(heap[i], heap[maxInd]);
-      siftDown(maxInd);
+      if (lChild < heap_size && compare(heap[maxInd], heap[lChild]))
+        maxInd = lChild;
+      
+      if (rChild < heap_size && compare(heap[maxInd], heap[rChild]))
+        maxInd = rChild;
+
+      if (i != maxInd) {
+        swap(heap[i], heap[maxInd]);
+        i = maxInd;
+      } else break;
     }
   }
 
@@ -88,6 +93,52 @@ private:
   Predicate compare = Predicate();
 };
 
+template<typename T>
+void siftDown(T begin, T end, size_t i, size_t sz,
+  function<bool(decltype(*begin), decltype(*begin))> compare = less<decltype(*begin)>()) {
+  
+  while (2*i+1 < sz) {
+    size_t maxInd = i;
+    size_t l = 2*i+1;
+    size_t r = 2*i+2;
+
+    if (l < sz && compare(*(begin + maxInd), *(begin + l)))
+      maxInd = l;
+    if (r < sz && compare(*(begin + maxInd), *(begin + r)))
+      maxInd = r;
+    
+    if (i != maxInd) {
+      swap(*(begin + i), *(begin + maxInd));
+      i = maxInd;
+    }
+    else break;
+  }
+}
+
+template<typename T>
+void heapify(T begin, T end,
+  function<bool(decltype(*begin), decltype(*begin))> compare = less<decltype(*begin)>()) {  
+  
+  size_t n = std::distance(begin, end);
+  for (size_t j = (n - 1)/2 + 1; j > 0; --j) {
+    siftDown(begin, end, j-1, n, compare);
+  }
+}
+
+template<typename T>
+void heap_sort(T begin, T end,
+  function<bool(decltype(*begin), decltype(*begin))> compare = less<decltype(*begin)>()) {  
+  
+  heapify(begin, end, compare);
+  size_t heap_size = std::distance(begin, end);
+  size_t counter = heap_size - 1;
+  for (size_t j = 0; j < heap_size; ++j) {
+    swap(*begin, *(begin + counter));
+    siftDown(begin, end, 0, counter, compare);
+    --counter;
+  }
+}
+
 } // custom namespace
 
-#endif // PRIORITY_QUEUE_H
+#endif // BINARY_HEAP_H
